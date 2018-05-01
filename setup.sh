259c41892ef6f53528f1a7fa87b499c8fb34fc7c
Naoaki Iwakiri <naokiri@gmail.com>
2018-05-01 14:54:12 +0900

    Add gitignore

--- setup.sh
+++ setup.sh
@@ -17,22 +17,44 @@ if [ ! -f ~/.vimrc ]; then
     ln -s $BASEDIR/dot.vimrc ~/.vimrc
 fi
 
+
+# Check if a git config entry alreay contains the file and add to global config
+#
+# $1: git config entry name e.g. include.path
+# $2: filename to add to git config
+# 
+git_config_add_once() {
+    if [ -z "$1" -o -z "$2" ]; then
+        echo "git_config_add_once call failed"
+        exit 1
+    fi
+
+    ENTRY_NAME=$1
+    TARGET_FILENAME=$2
+
+    GIT_EXISTS_FLAG=0
+    set +e
+    GIT_INCLUDE_LIST=$(git config --global --get-all $ENTRY_NAME)
+    set -e
+    if [ -n "$GIT_INCLUDE_LIST" ]; then
+        for GIT_INCLUDE_FILE in ${GIT_INCLUDE_LIST}; do
+            if [ "$GIT_INCLUDE_FILE" = "$TARGET_FILENAME" ]; then
+                GIT_EXISTS_FLAG=1
+            fi
+        done
+    fi
+    if [ $GIT_EXISTS_FLAG -eq 0 ]; then
+        echo "Setup" $TARGET_FILENAME
+        git config --global --add $ENTRY_NAME $TARGET_FILENAME
+    fi
+}
+
+
 # gitconfig
-GIT_TARGET_FILE="$BASEDIR/gitconfig"
-GIT_DONE=0
-set +e
-GIT_INCLUDE_LIST=$(git config --global --get-all include.path)
-set -e
-if [ "$GIT_INCLUDE_LIST" != "" ]; then
-    for GIT_INCLUDE_FILE in ${GIT_INCLUDE_LIST}; do
-        if [ "$GIT_INCLUDE_FILE" = "$GIT_TARGET_FILE" ]; then
-            GIT_DONE=1
-        fi
-    done
-fi
-if [ $GIT_DONE -eq 0 ]; then
-    echo "Setup gitconfig"
-    git config --global --add include.path "$BASEDIR/gitconfig"
-fi
+git_config_add_once "include.path" "$BASEDIR/gitconfig"
+
+
+# gitignore
+git_config_add_once "core.excludesfile" "$BASEDIR/gitignore"
 
 echo "Done!"
