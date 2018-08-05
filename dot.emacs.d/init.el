dc14966478f3a506475e8be5cd3b0c71448b9711
Naoaki Iwakiri <naokiri@gmail.com>
2018-08-05 12:02:28 +0900

    Use straight to manage emacs

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -5,55 +5,49 @@
 ;;
 ;; necessary configs for editing inits
 ;;
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq-default indent-tabs-mode nil)
 (global-set-key (kbd "\C-h") 'delete-backward-char)
 (global-set-key (kbd "\C-x ?") 'help)
 (global-set-key (kbd "\C-t") 'ESC-prefix)
 
 ;;
-;; environment
+;; package environment
 ;;
-(require 'package)
+(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+      (bootstrap-version 3))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")
-        ("org" . "http://orgmode.org/elpa/")))
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
-(package-initialize)
+;; (setq syncing-packages
+;;       '(
+;;         init-loader
+;;         package-utils
+;;         ddskk
+;;         undo-tree
+;;         org-journal
+;;         which-key
+;;         ))
 
-(setq syncing-packages
-      '(
-        init-loader
-        package-utils
-        ddskk
-        undo-tree
-        org-journal
-        which-key
-        ))
+;(dolist (package-name syncing-packages)
+;  (straight-use-package package-name))
 
-(when (not package-archive-contents) (package-refresh-contents))
 
-(dolist (pkg syncing-packages)
-    (when (and (not (package-installed-p pkg)) (assoc pkg package-archive-contents))
-        (package-install pkg)))
+(use-package init-loader)
+(init-loader-load "~/.emacs.d/inits")
 
-;; TODO?: pacakage-list-packages U,x update all packages, automatically here?
-;; TODO?: package-utils-upgrade-by-name init-loader?
-(init-loader-load)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (restart-emacs org-journal which-key undo-tree ddskk package-utils init-loader))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
---
+++ dot.emacs.d/straight/versions/default.el
@@ -0,0 +1,7 @@
+(("elpa" . "7ba1bdad96db3252e5c2a0ddd88c52f5b075ca9e")
+ ("epkgs" . "2fbb0caedfc168000251238e6ce49f39ceae32ba")
+ ("init-loader" . "5d3cea1004c11ff96b33020e337b03b925c67c42")
+ ("melpa" . "1217e0d4f42df68cc22de9b4f27a36c0377509e3")
+ ("straight.el" . "b71d39f9fffae089bc5df1c9ed2bbbfd83275256")
+ ("use-package" . "3fb8f39f5901a4c0ef7887283e56e60b541675ea"))
+:jupiter
