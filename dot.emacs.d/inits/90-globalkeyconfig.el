4d739b77d83fd2737f5892289de6f7169bc7bb59
Naoaki Iwakiri <naoaki@indeed.com>
2019-03-10 14:37:21 +0900

    Indent file like intellij

--- dot.emacs.d/inits/90-globalkeyconfig.el
+++ dot.emacs.d/inits/90-globalkeyconfig.el
@@ -1,3 +1,10 @@
 (global-set-key (kbd "C-x C-j") 'skk-mode)
 (global-set-key (kbd "M-/") 'undo-tree-redo)
 (global-set-key (kbd "C-/") 'comment-or-uncomment-region)
+
+(defun indent-buffer ()
+  (interactive)
+  (save-excursion
+    (indent-region (point-min) (point-max) nil)))
+
+(global-set-key (kbd "C-x C-l") 'indent-buffer)
